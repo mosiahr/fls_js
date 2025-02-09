41e8a1a4576a4c0bceb5af0835092e8e1d565a10
Gregory <greg.mosia@gmail.com>
2025-02-09 23:59:22 +0000

    L3: Solve Task 3

--- lesson_3/js/script3.js
+++ lesson_3/js/script3.js
@@ -1,15 +1,49 @@
 "use strict"
 
+const HOMEWORK_NUMBER = 3
 const TASK_NUMBER = 3
+const NOT_CORRECTED_NUMBER =
+  "Sorry, you entered not corrected number.\nYou need to enter a number between 1 and 5."
 
-const valueOneProduct = parseFloat(
-  prompt("Enter value of the product, please", 0)
+const minNumber = 1
+const maxNumber = 5
+
+const randomNumber =
+  minNumber + Math.floor(Math.random() * (maxNumber - minNumber + 1))
+
+const firstAttemptNumber = parseInt(
+  prompt(
+    "A number from 1 to 5 is randomly generated. \nFirst attempt. \nEnter your number: "
+  )
 )
-const productVolume = parseInt(
-  prompt("Enter volume of the products, please", 0)
-)
-const totalValue = valueOneProduct * productVolume
-const vat = totalValue * 0.05
+
+let result = ""
+let inputDataSecondAttemptNumber = ""
+
+if (firstAttemptNumber > 0 && firstAttemptNumber < 6) {
+  if (randomNumber === firstAttemptNumber) {
+    result = "You guessed the number!"
+  } else {
+    const secondAttemptNumber = parseInt(
+      prompt(
+        "Sorry! You don't guess the number! \nYou have one more attempt. \nSecond attempt. \nEnter your number: "
+      )
+    )
+
+    if (secondAttemptNumber > 0 && secondAttemptNumber < 6) {
+      if (randomNumber === secondAttemptNumber) {
+        result = "You guessed the number!"
+      } else {
+        result = "Sorry! You don't guess the number!"
+      }
+      inputDataSecondAttemptNumber = `<li>Your second number = ${secondAttemptNumber}</li>`
+    } else {
+      result = NOT_CORRECTED_NUMBER
+    }
+  }
+} else {
+  result = NOT_CORRECTED_NUMBER
+}
 
 document.write(`
 <body>
@@ -18,7 +52,7 @@ document.write(`
 			<div class="header__container">
 				<div class="header__block">
 					<a href="../../index.html"><img class="img-home" src="../../img/logo.webp" alt="Home"></a>
-					<h1><a class="header__link" href="../index.html">Homework 2</a> / Task ${TASK_NUMBER}</h1>
+					<h1><a class="header__link" href="../index.html">Homework ${HOMEWORK_NUMBER}</a> / Task ${TASK_NUMBER}</h1>
 				</div>
 			</div>
 		</header>
@@ -28,16 +62,16 @@ document.write(`
 					<ol class="page__list">
 						<li>
 							<h3>Task ${TASK_NUMBER}</h3>
-							<p>Дано вартість одиниці товару і кількість: Знайти загальну вартість та ПДВ (5% від загальної
-								вартості).</p>
+							<p>Випадковим чином генерується число від 1 до 5. Спробуйте вгадати число
+								за 2 спроби.</p>
 						</li>
 						<li>
 							<h3>Solution of Task ${TASK_NUMBER}</h3>
 							<ol>
-								<li>Value of One Product: ${valueOneProduct.toFixed(2)}</li>
-								<li>Product Volume: ${productVolume}</li>
-								<li>Total Volume: ${totalValue.toFixed(2)}</li>
-								<li>VAT (5%): ${vat.toFixed(2)}</li>
+								<li>Your first number = ${firstAttemptNumber}</li>
+								${inputDataSecondAttemptNumber}
+								<li>Random Number: ${randomNumber}</li>
+								<li>${result}</li>
 							</ol>
 						</li>
 						<li class="reload">
