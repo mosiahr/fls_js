d794ece1c2b311d15d9466bf7a7112542885064a
Gregory <greg.mosia@gmail.com>
2025-02-19 15:54:43 +0000

    L7: Solve Task 0

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict"
+
+export function confirmBeginTest(funcTest) {
+  if (confirm("Begin test?")) {
+    funcTest()
+  }
+}
+
+export const randomNumber = (minNumber, maxNumber) =>
+  minNumber + Math.floor(Math.random() * (maxNumber - minNumber + 1))
+
+export function renderTask(
+  homeworkNumber,
+  taskNumber,
+  taskDefinition,
+  renderResult
+) {
+  const bodyEl = document.body
+  bodyEl.innerHTML = `
+		<body>
+			<div class="wrapper">
+				<header class="header">
+					<div class="header__container">
+						<div class="header__block">
+							<a href="../../index.html"><img class="img-home" src="../../img/logo.webp" alt="Home"></a>
+							<h1><a class="header__link" href="../index.html">Homework ${homeworkNumber}</a> / Task ${taskNumber}</h1>
+						</div>
+					</div>
+				</header>
+				<main class="page">
+					<div class="page__container">
+						<div class="page-block">
+							<ol class="page__list">
+								<li>
+									<h3>Task ${taskNumber}</h3>
+									<p>${taskDefinition}</p>
+								</li>
+								<li>
+									<h3>Solution of Task ${taskNumber}</h3>
+									${renderResult}
+								</li>
+								<li class="reload">
+									<a href="task${taskNumber}.html"><img src="../../img/circular-arrow.svg" alt="Reload"></a>
+								</li>
+							</ol>
+		
+						</div>
+					</div>
+				</main>
+				<footer class="footer">
+					<div class="footer__container">
+						<span>© 2025 Gregory Mosia</span>
+					</div>
+				</footer>
+			</div>
+		</body>`
+}
---
+++ lesson_7/js/script0.js
@@ -0,0 +1,55 @@
+"use strict"
+
+import { confirmBeginTest, randomNumber, renderTask } from "../../js/script.js"
+
+const HOMEWORK_NUMBER = 7
+const TASK_NUMBER = 0
+const TASK_DEFINITION =
+  "Створити функцію, яка за номером місяця повертає пору року, до якої відноситься цей місяць."
+const MESSAGE_NOT_CORRECTED_NUMBER =
+  "Sorry, you entered not corrected number.\nYou need to enter a number from 1 to 12."
+
+const getMonthNumber = () =>
+  parseInt(prompt("Enter your month number (1-12): ", randomNumber(1, 12)))
+
+function isFoundNumberAmongThreeElements(number, element1, element2, element3) {
+  if (number === element1 || number === element2 || number === element3) {
+    return true
+  }
+  return false
+}
+
+function getSeason(monthNumber) {
+  let result
+  switch (true) {
+    case isFoundNumberAmongThreeElements(monthNumber, 12, 1, 2):
+      result = "Winter"
+      break
+    case isFoundNumberAmongThreeElements(monthNumber, 3, 4, 5):
+      result = "Spring"
+      break
+    case isFoundNumberAmongThreeElements(monthNumber, 6, 7, 8):
+      result = "Summer"
+      break
+    case isFoundNumberAmongThreeElements(monthNumber, 9, 10, 11):
+      result = "Autumn"
+      break
+    default:
+      result = MESSAGE_NOT_CORRECTED_NUMBER
+      break
+  }
+  return result
+}
+
+const main = () => {
+  const monthNumber = getMonthNumber()
+  const season = getSeason(monthNumber)
+  const renderResult = `<ol>
+		<li>Your month number = ${monthNumber}</li>
+		<li>Season: ${season}</li>
+	</ol>`
+
+  renderTask(HOMEWORK_NUMBER, TASK_NUMBER, TASK_DEFINITION, renderResult)
+}
+
+confirmBeginTest(main)
