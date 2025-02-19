a7d6452a2c3de5bdba4b617c5fab4730f22e0979
Gregory <greg.mosia@gmail.com>
2025-02-19 18:11:48 +0000

    L7: Solve Task 1, Task2

---
+++ lesson_7/js/script1.js
@@ -0,0 +1,65 @@
+"use strict"
+
+import { confirmBeginTest, randomNumber, renderTask } from "../../js/script.js"
+
+const HOMEWORK_NUMBER = 7
+const TASK_NUMBER = 1
+const TASK_DEFINITION =
+  "Створити функцію, яка за номером місяця повертає його назву."
+const MESSAGE_NOT_CORRECTED_NUMBER =
+  "Sorry, you entered not corrected number.\nYou need to enter a number from 1 to 12."
+
+const getMonthNumber = () =>
+  parseInt(prompt("Enter your month number (1-12): ", randomNumber(1, 12)))
+
+function getMonthName(monthNumber) {
+  let result = ""
+
+  if (monthNumber < 1 || monthNumber > 12) result = MESSAGE_NOT_CORRECTED_NUMBER
+  else if (monthNumber === 1) result = "January"
+  else if (monthNumber === 2) result = "February"
+  else if (monthNumber === 3) result = "March"
+  else if (monthNumber === 4) result = "April"
+  else if (monthNumber === 5) result = "May"
+  else if (monthNumber === 6) result = "June"
+  else if (monthNumber === 7) result = "July"
+  else if (monthNumber === 8) result = "Augest"
+  else if (monthNumber === 9) result = "September"
+  else if (monthNumber === 10) result = "October"
+  else if (monthNumber === 11) result = "November"
+  else if (monthNumber === 12) result = "December"
+
+  return result
+}
+
+const renderResultGetMonthName = (monthNumber) => {
+  let result = ""
+  if (isNaN(monthNumber)) {
+    result = `Massage: ${MESSAGE_NOT_CORRECTED_NUMBER}`
+  } else {
+    if (monthNumber < 1 || monthNumber > 12) {
+      result = `Massage: ${MESSAGE_NOT_CORRECTED_NUMBER}`
+    } else {
+      const monthName = getMonthName(monthNumber)
+      result = `Month Name: ${monthName}`
+    }
+  }
+
+  return `<ol>
+			<li>Month Number = ${monthNumber}</li>
+			<li>${result}</li>
+		</ol>`
+}
+
+const main = () => {
+  const monthNumber = getMonthNumber()
+
+  renderTask(
+    HOMEWORK_NUMBER,
+    TASK_NUMBER,
+    TASK_DEFINITION,
+    renderResultGetMonthName(monthNumber)
+  )
+}
+
+confirmBeginTest(main)
---
+++ lesson_7/js/script2.js
@@ -0,0 +1,46 @@
+"use strict"
+
+import { confirmBeginTest, randomNumber, renderTask } from "../../js/script.js"
+
+const HOMEWORK_NUMBER = 7
+const TASK_NUMBER = 2
+const TASK_DEFINITION =
+  "Створити функцію, яка за номером дня дозволяє зʼясувати чи є цей день робочим."
+const MESSAGE_NOT_CORRECTED_NUMBER =
+  "Sorry, you entered not corrected number.\nYou need to enter a number from 1 to 7."
+
+const getDayNumber = () =>
+  parseInt(prompt("Enter your day number (1-7): ", randomNumber(1, 7)))
+
+const isWorkingDay = (dayNumber) => dayNumber >= 1 && dayNumber <= 5
+
+const renderResultIsWorkingDay = (dayNumber) => {
+  let result = ""
+  if (isNaN(dayNumber)) {
+    result = `Massage: ${MESSAGE_NOT_CORRECTED_NUMBER}`
+  } else {
+    if (dayNumber < 1 || dayNumber > 7) {
+      result = `Massage: ${MESSAGE_NOT_CORRECTED_NUMBER}`
+    } else {
+      result = `Is this day a working day?: ${isWorkingDay(dayNumber)}`
+    }
+  }
+
+  return `<ol>
+			<li>Day Number = ${dayNumber}</li>
+			<li>${result}</li>
+		</ol>`
+}
+
+const main = () => {
+  const dayNumber = getDayNumber()
+
+  renderTask(
+    HOMEWORK_NUMBER,
+    TASK_NUMBER,
+    TASK_DEFINITION,
+    renderResultIsWorkingDay(dayNumber)
+  )
+}
+
+confirmBeginTest(main)
