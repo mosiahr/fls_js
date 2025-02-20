5c92e93655fd8f6cf980ceacf7da90d902216057
Gregory <greg.mosia@gmail.com>
2025-02-20 19:57:16 +0000

    L7: Fix Task 7

--- lesson_7/js/script7.js
+++ lesson_7/js/script7.js
@@ -7,11 +7,7 @@ const TASK_NUMBER = 7
 const TASK_DEFINITION = `Створити функцію, яка випадковим чином виводить на екран одне із 4 зображень (шляхи до зображень
 								передаються у функцію).`
 
-const getRandomPath = (path1, path2, path3, path4) => {
-  const pathArr = [path1, path2, path3, path4]
-  const number = getRandomNumber(0, pathArr.length - 1)
-  return pathArr[number]
-}
+const getRandomPath = (...path) => path[getRandomNumber(0, path.length - 1)]
 
 const renderResult = (path) => {
   return `<ol><li>${path}</li></ol><ol><li>Random Image:  <img src="${path}" alt="Image"></li></ol>`
