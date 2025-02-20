0c8c9c92ec8d2cf961061666c2c4f9ab59dfa77d
Gregory <greg.mosia@gmail.com>
2025-02-20 16:19:15 +0000

    L7: Solve Task 8

---
+++ lesson_7/js/script8.js
@@ -0,0 +1,40 @@
+"use strict"
+
+import { confirmBeginTest, randomNumber, renderTask } from "../../js/script.js"
+
+const HOMEWORK_NUMBER = 7
+const TASK_NUMBER = 8
+const TASK_DEFINITION = `Створити функцію, яка виводить банер (у функцію передаються: зображення, заголовок та посилання,
+								куди переходимо при кліку на зображення (тег img повине знаходитись у середині
+								тега a: &lta&gt &ltimg src="шлях"&gt &lt/a&gt)`
+
+const getRandomPath = (path1, path2, path3, path4) => {
+  const pathArr = [path1, path2, path3, path4]
+  const number = randomNumber(0, pathArr.length - 1)
+  return pathArr[number]
+}
+
+const renderBanner = (src, title, href) => `
+		<div class="banner">
+			<h3>${title}</h3>
+			<a href="${href}" target="_blank"><img src="${src}" alt="Image"></a>
+		</div>
+	`
+
+const main = () => {
+  const src = getRandomPath(
+    "../img/facebook.svg",
+    "../img/instagram.svg",
+    "../img/whatsapp.svg",
+    "../img/youtube.svg"
+  )
+
+  renderTask(
+    HOMEWORK_NUMBER,
+    TASK_NUMBER,
+    TASK_DEFINITION,
+    renderBanner(src, "Function", "https://uk.javascript.info/function-basics")
+  )
+}
+
+confirmBeginTest(main)
