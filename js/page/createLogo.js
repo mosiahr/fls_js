7f37b4590f274404156473de4a16ae2660acbfce
Gregory <greg.mosia@gmail.com>
2025-02-24 17:26:48 +0000

    L8: Fix paths

--- js/page/createLogo.js
+++ js/page/createLogo.js
@@ -1,6 +1,5 @@
 export default function createLogo() {
-    return `
-    <a href="index.html"><img class="img-home" src="../../img/logo.webp" alt="Home"></a>
+  return `
+    <a href="../index.html"><img class="img-home" src="../img/logo.webp" alt="Home"></a>
     `
 }
-
