12f74907bf2595d475ed83d3743ea2e746657ba0
Gregory <greg.mosia@gmail.com>
2025-02-24 06:00:17 +0000

    L8: fix createLogo import path

--- js/page/createLogo.js
+++ js/page/createLogo.js
@@ -1,6 +1,6 @@
 export default function createLogo() {
     return `
-    <a href="../../index.html"><img class="img-home" src="/img/logo.webp" alt="Home"></a>
+    <a href="index.html"><img class="img-home" src="../../img/logo.webp" alt="Home"></a>
     `
 }
 
