bfc985b1d991d272f37e8fad407e90be547e3e86
Gregory <greg.mosia@gmail.com>
2025-05-11 06:25:09 +0100

    Main: Clean

--- js/components/createLink.js
+++
@@ -1,6 +0,0 @@
-export default function createLink(...elements) {
-    return `<a class="item-homework" href="lesson_2/index.html">
-				<h3>Homework 2</h3>
-				<p>Вступ до веб-програмування. Вступ до JS. Лінійні алгоритми.</p>
-			</a>`
-}
