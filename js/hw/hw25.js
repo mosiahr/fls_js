e669afb168ebd18733706d5aa701035e729b0a30
Gregory <greg.mosia@gmail.com>
2025-05-18 23:16:22 +0100

    L25: Task #1: Add jooble API

--- js/hw/hw25.js
+++ js/hw/hw25.js
@@ -17,3 +17,50 @@ const lesson = getNumbersFromCurrentFileName(import.meta)
 // Намагайтесь зробити це з використанням класів. Окремо клас для побудови розмітки.
 //  Окремо клас, який буде робити запити і повертати результати.
 
+function joobleAPI() {
+  var url = 'https://jooble.org/api/'
+  var key = '20360c7a-770d-443a-9803-9d239555aeee'
+  var params = "{ keywords: 'it, front-end', location: ''}"
+
+  //create xmlHttpRequest object
+  var http = new XMLHttpRequest()
+  //open connection. true - asynchronous, false - synchronous
+  http.open('POST', url + key, true)
+
+  //Send the proper header information
+  http.setRequestHeader('Content-type', 'application/json')
+
+  //Callback when the state changes
+  http.onreadystatechange = function () {
+    if (http.readyState == 4 && http.status == 200) {
+      console.log(http.responseText)
+    }
+  }
+  //Send request to the server
+  const req = http.send(params)
+  return req
+}
+
+class Fetch {
+	async fetchData(path){
+		const res = await fetch(path)
+		const {status} = res
+
+		if (status < 200 || status >=300){
+			return 
+		}
+
+	}
+}
+
+export function task1_25() {}
+
+task1_25.solutionParams = {
+  code: trunsformEntityToCode(task1_25),
+  name: '',
+  title: '',
+  lesson,
+  task: 1,
+  params: [],
+  resultAsCode: false,
+}
